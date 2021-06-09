087b298f3ea7760cd96c74c4200945ca28d7abdd
Tsquare25 <tibebe1234t@gmail.com>
2021-06-09 13:35:20 +0100

    Added a profile page

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # create a relationship with the user model 
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    bio = models.TextField(editable=False)
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,7 +1,10 @@
+from django.dispatch.dispatcher import receiver
 from django.shortcuts import redirect, render
 from django.contrib import messages
-from .form import UserRegisterFrom
+from .form import UserProfileForm, UserRegisterFrom, UserUpdateForm
 from django.contrib.auth.decorators import login_required
+
+
 def register(request):
 
     if request.method == "POST":
@@ -18,10 +21,25 @@ def register(request):
     context = {'form':form}
     return render(request, 'users/register.html', context )
 
-def login(request):
-    pass
+
 
 @login_required
 def profile(request):
-    context = {}
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST)
+        p_form = UserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your profile has been Updated ')
+            redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = UserProfileForm(instance=request.user.profile)
+
+    context = {
+        'u_form' : u_form,
+        'p_form' : p_form
+    }
     return render(request, 'users/profile.html', context)
